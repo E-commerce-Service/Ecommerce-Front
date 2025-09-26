3a8907572a951849fb9a5c1a37c2bfa6592fb2b1
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:28:55 -0300

    style: Add global styles, interceptors and image loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,42 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+   ApplicationConfig,
+   provideBrowserGlobalErrorListeners, Provider,
+   provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {CookieService} from 'ngx-cookie-service';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {authInterceptor} from './core/interceptors/auth.interceptor';
+import {IMAGE_LOADER, ImageLoaderConfig} from '@angular/common';
+
+const customImageLoader = (config: ImageLoaderConfig) => {
+   if (config.src.includes('assets/')) {
+      return config.src;
+   }
+   const cloudinaryUrl = 'https://res.cloudinary.com/dytzru3ad/image/upload/';
+   const params = config.width ? `w_${config.width},q_auto,f_auto` : 'q_auto,f_auto';
+   const imageName = config.src.startsWith('/') ? config.src.substring(1) : config.src;
+
+   return `${cloudinaryUrl}${params}/${imageName}`;
+};
+
+export const customImageLoaderProvider: Provider = {
+   provide: IMAGE_LOADER,
+   useValue: customImageLoader,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+   providers: [
+      provideHttpClient(
+         withInterceptors([authInterceptor]),
+         withFetch()
+      ),
+      customImageLoaderProvider,
+      provideBrowserGlobalErrorListeners(),
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      CookieService,
+   ],
 };
