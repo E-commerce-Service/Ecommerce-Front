9786c68770635a1bca0cf536ed1194715671cac2
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:25:42 -0300

    feat: Add shared button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {NgClass, NgOptimizedImage} from '@angular/common';
+
+@Component({
+   selector: 'app-button',
+   imports: [NgClass, NgOptimizedImage],
+   templateUrl: './button.component.html',
+   styleUrl: './button.component.sass',
+})
+export class ButtonComponent {
+   @Input() label!: string;
+   @Input() type: 'button' | 'submit' | 'reset' = 'button';
+   @Input() disabled = false;
+   @Input() color = 'bg-blue-base';
+   @Input() hover = 'hover:bg-blue-dark';
+   @Input() icon!: string
+
+   @Output() clicked = new EventEmitter<void>();
+
+   onClick() {
+      if (!this.disabled) {
+         this.clicked.emit();
+      }
+   }
+}
