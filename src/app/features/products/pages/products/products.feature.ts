f92765b2a7aacf8c13bfe86a8b7ebec1430d377a
Wesley00s <wesley300rodrigues@gmail.com>
2025-11-22 20:07:36 -0300

    feat: Add ProductsFeature for displaying product details and reviews

---
+++ src/app/features/products/pages/products/products.feature.ts
@@ -0,0 +1,38 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductDetailsComponent } from '../../components/product-details/product-details.component';
+import { ReviewsComponent } from '../../components/reviews/reviews.component';
+import { ActivatedRoute } from '@angular/router';
+import {
+   ProductService,
+   ProductState,
+} from '../../../../core/services/product.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { InfoContainerComponent } from '../../../../shared/components/info-container/info-container.component';
+import { LoadingContainerComponent } from '../../../../shared/components/loading-container/loading-container.component';
+
+@Component({
+   selector: 'app-products',
+   imports: [
+      ProductDetailsComponent,
+      ReviewsComponent,
+      AsyncPipe,
+      InfoContainerComponent,
+      LoadingContainerComponent,
+   ],
+   templateUrl: './products.feature.html',
+   styleUrl: './products.feature.sass',
+})
+export class ProductsFeature implements OnInit {
+   protected route = inject(ActivatedRoute);
+   protected productService = inject(ProductService);
+   protected productCode!: string;
+
+   readonly productState$: Observable<ProductState> =
+      this.productService.productState$;
+
+   ngOnInit(): void {
+      this.productCode = this.route.snapshot.paramMap.get('code')!;
+      this.productService.getProductByCode(this.productCode);
+   }
+}
