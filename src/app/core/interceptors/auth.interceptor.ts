4c5658fbd70d5f11eb4b138b175f6dc2591322c6
Wesley00s <wesley300rodrigues@gmail.com>
2025-11-22 19:58:44 -0300

    feat: Refactor auth interceptor for improved request handling and error management

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,42 +1,50 @@
-import {HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
-import {inject} from '@angular/core';
-import {catchError, Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
-
+import {
+   HttpErrorResponse,
+   HttpEvent,
+   HttpHandlerFn,
+   HttpInterceptorFn,
+   HttpRequest,
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { environment } from '../../../environment/enviroment';
+import { CookieService } from 'ngx-cookie-service';
+import { SKIP_AUTH_REDIRECT } from '../tokens/auth.context';
 
 export const authInterceptor: HttpInterceptorFn = (
    request: HttpRequest<unknown>,
-   next: HttpHandlerFn
+   next: HttpHandlerFn,
 ): Observable<HttpEvent<unknown>> => {
    const router = inject(Router);
-   const token = localStorage.getItem('token');
-   const baseURL = 'http://localhost:8080';
-   const protectedRoutes = [`${baseURL}/auth/me`, `${baseURL}/wallet`, `${baseURL}/brokerage`];
-
-   const isProtectedRoute = protectedRoutes.some((url) =>
-      request.url.startsWith(url)
-   );
+   const cookieService = inject(CookieService);
+   const apiUrl = environment.apiUrl;
 
    let processedRequest = request;
 
-   if (token && isProtectedRoute) {
+   if (request.url.startsWith(apiUrl)) {
       processedRequest = request.clone({
-         setHeaders: {
-            Authorization: `Bearer ${token}`,
-         },
+         withCredentials: true,
       });
    }
 
    return next(processedRequest).pipe(
       catchError((error: unknown) => {
-         if (error instanceof HttpErrorResponse && error.status === 401) {
-            localStorage.removeItem('token');
+         if (
+            error instanceof HttpErrorResponse &&
+            (error.status === 401 || error.status === 403)
+         ) {
+            if (request.context.get(SKIP_AUTH_REDIRECT)) {
+               return throwError(() => error);
+            }
+
+            cookieService.delete('token', '/');
             router.navigate(['/login']).then(() => {
                window.location.reload();
             });
          }
 
          return throwError(() => error);
-      })
+      }),
    );
 };
