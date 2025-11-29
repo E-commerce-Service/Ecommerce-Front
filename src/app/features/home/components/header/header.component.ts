b1ac01b66bc28dcf79364db4d8b88f708ef8dc5c
Wesley00s <wesley300rodrigues@gmail.com>
2025-11-29 00:23:12 -0300

    feat: Add navigation to products from HeaderComponent

--- src/app/features/home/components/header/header.component.ts
+++ src/app/features/home/components/header/header.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
    selector: 'app-header',
-   imports: [ButtonComponent, NgOptimizedImage],
+   imports: [NgOptimizedImage],
    templateUrl: './header.component.html',
    styleUrl: './header.component.sass',
 })
 export class HeaderComponent {
    protected phone = 'assets/images/phone.svg';
    protected phoneAlt = 'Phone';
+   currentYear = new Date().getFullYear();
+
+   @Output() navigateToProducts = new EventEmitter<void>();
 
    onButtonClick() {
-      return;
+      this.navigateToProducts.emit();
    }
 }
